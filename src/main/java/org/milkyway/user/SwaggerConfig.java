def59548d5b0b3a0a633282cfecaaebe723a45bd
Prashanth H1 <PH00487322@techmahindra.com>
2019-03-12 19:32:59 +0530

    Changed swagger configuration

--- src/main/java/org/milkyway/user/SwaggerConfig.java
+++ src/main/java/org/milkyway/user/SwaggerConfig.java
@@ -3,6 +3,7 @@ package org.milkyway.user;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,7 +16,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 @Bean
 public Docket api() {
-	return new Docket(DocumentationType.SWAGGER_2);
+	return new Docket(DocumentationType.SWAGGER_2)
+	.select()
+    .apis(RequestHandlerSelectors.basePackage("org.milkyway.controller"))
+    .build();
 }
 
 }
